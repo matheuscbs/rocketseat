ef0ed83b7bf03708d0e81be71962c41d48f3f606
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-07-15 12:49:46 -0300

    improvement styles

--- 02-ignite-timer/src/styles/themes/default.ts
+++ 02-ignite-timer/src/styles/themes/default.ts
@@ -1,5 +1,21 @@
 export const defaultTheme = {
-  primary: "#8257e6",
-  secondary: "green",
   white: "#fff",
+
+  "gray-100": "#E1E1E6",
+  "gray-300": "#c4c4cc",
+  "gray-400": "#8d8d99",
+  "gray-500": "#7c7c84",
+  "gray-600": "#323238",
+  "gray-700": "#29292e",
+  "gray-800": "#202024",
+  "gray-900": "#121214",
+
+  "green-300": "#00b37e",
+  "green-500": "#00875f",
+  "green-700": "#015f43",
+
+  "red-500": "#ab222e",
+  "red-700": "#7a1921",
+
+  "yellow-500": "#fba94c",
 };
