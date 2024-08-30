2bf4480215f13d82a05c0177dfea5f8b80cbf0c5
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-08-29 23:28:14 -0300

    feat: transactions table

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,71 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de website</td>
+              <td>
+                <PriceHighLight variant="income">R$12.000</PriceHighLight>
+              </td>
+              <td>Desenvolvimento</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de website</td>
+              <td>
+                <PriceHighLight variant="income">R$12.000</PriceHighLight>
+              </td>
+              <td>Desenvolvimento</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td>Aluguel</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$1.100</PriceHighLight>
+              </td>
+              <td>Casa</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td>Aluguel</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$1.100</PriceHighLight>
+              </td>
+              <td>Casa</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de website</td>
+              <td>
+                <PriceHighLight variant="income">R$12.000</PriceHighLight>
+              </td>
+              <td>Desenvolvimento</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td>Aluguel</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$1.100</PriceHighLight>
+              </td>
+              <td>Casa</td>
+              <td>10/02/2021</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
