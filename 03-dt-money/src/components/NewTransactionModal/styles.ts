2bf85f5f1691e4315f14365f15296f1a0b4cb66a
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-09-10 16:15:42 -0300

    feat: Radio Button Selection Transaction Type

--- 03-dt-money/src/components/NewTransactionModal/styles.ts
+++ 03-dt-money/src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 import styled from "styled-components";
 
 export const Overlay = styled(Dialog.Overlay)`
@@ -70,7 +71,7 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme["gray-500"]};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -81,7 +82,9 @@ interface TransactionTypeButtonProps {
   variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(
+  RadioGroup.Item
+)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
@@ -96,5 +99,16 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
   svg {
     color: ${(props) =>
       props.theme[props.variant === "income" ? "green-300" : "red-300"]};
+  }
+
+  &[data-state="unchecked"]:hover {
+    transition: background-color 0.2s;
+    background: ${(props) => props.theme["gray-600"]};
+  }
+
+  &[data-state="checked"] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.theme[props.variant === "income" ? "green-500" : "red-500"]};
   }
 `;
