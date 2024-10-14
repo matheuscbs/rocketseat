ff07a3d6c9cc8d6d30d41d1f8a66079db81b45b5
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-10-13 21:32:13 -0300

    feat: global styles stitches css

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/globals";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
