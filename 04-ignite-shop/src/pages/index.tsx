fae3ca656b733416b6d0e64d1ec6d91d6a30aa81
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-10-16 18:31:07 -0300

    feat: home products

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,25 +1,27 @@
-import { styled } from "../styles";
-
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-
-  span: {
-    fontWeight: "bold",
-  },
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import Image from "next/image";
+import camiseta1 from "../assets/shirts/1.png";
+import camiseta2 from "../assets/shirts/2.png";
+import { HomeContainer, Product } from "../styles/pages/home";
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
