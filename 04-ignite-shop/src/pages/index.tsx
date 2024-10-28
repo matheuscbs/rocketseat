747343068851e8a057d893ff8d47616004d858f2
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-10-27 22:57:59 -0300

    feat: carrousel

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,12 +1,23 @@
+import "keen-slider/keen-slider.min.css";
+
+import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
 import camiseta1 from "../assets/shirts/1.png";
 import camiseta2 from "../assets/shirts/2.png";
+import camiseta3 from "../assets/shirts/3.png";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480} />
 
         <footer>
@@ -14,8 +25,24 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
 
         <footer>
           <strong>Camiseta X</strong>
