074a35b1b5c7501cbe7ceae257a716936ec5a296
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-11-04 12:31:22 -0300

    feat: static props nextjs

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -2,7 +2,7 @@ import "keen-slider/keen-slider.min.css";
 
 import axios from "axios";
 import { useKeenSlider } from "keen-slider/react";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import sharp from "sharp";
 import Stripe from "stripe";
@@ -66,7 +66,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -92,5 +92,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2h
   };
 };
