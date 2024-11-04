9580d624fa2a74345bff918edb07f0266a4ad989
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-11-04 13:46:37 -0300

    feat: styles and html product page

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,6 +1,27 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter();
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,99</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+          similique nam laboriosam adipisci voluptate quas velit omnis, tempore
+          id nisi commodi eos repellat tempora atque vitae quo impedit et
+          praesentium.
+        </p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
