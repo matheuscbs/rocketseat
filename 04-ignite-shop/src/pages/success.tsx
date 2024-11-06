e9530ce73d5288a213e935deb2a298e45f958026
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-11-05 23:04:34 -0300

    feat: success page functionallity

--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -1,17 +1,62 @@
+import { GetServerSideProps } from "next";
+import Image from "next/image";
 import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra Efetuada!</h1>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
       <p>
-        Uhull <strong>Matheus Cardoso</strong>, sua{" "}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhull <strong>{customerName}</strong>, sua{" "}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const customerName = session.customer_details?.name || "Cliente";
+
+  const productData = session.line_items?.data[0];
+  const product = productData?.price?.product as Stripe.Product | undefined;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product?.name || "Produto",
+        imageUrl: product?.images ? product.images[0] : "",
+      },
+    },
+  };
+};
