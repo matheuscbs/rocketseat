7096f1fb7fc09226601741f7645893b8fa40a6a4
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-11-09 17:11:45 -0300

    feat: configure dark theme and light theme selection storybook

--- 05-design-system/packages/docs/.storybook/preview.ts
+++ 05-design-system/packages/docs/.storybook/preview.ts
@@ -1,11 +1,18 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
+import darkTheme from "./darkTheme";
 
 const preview: Preview = {
   parameters: {
+    darkMode: {
+      current: "dark",
+      dark: darkTheme,
+      light: themes.light,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
   },
