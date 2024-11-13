cf037048d1f083739023019bf4de9346af5a0bfa
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-11-12 22:58:23 -0300

    feat: configure storybook with correct version

--- 05-design-system/packages/docs/vite.config.ts
+++ 05-design-system/packages/docs/vite.config.ts
@@ -1,9 +1,8 @@
+/// <reference types="vite/client" />
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".js", ".ts", ".tsx", ".jsx"], // Certifique-se de que .tsx está listado aqui
-  },
 });
